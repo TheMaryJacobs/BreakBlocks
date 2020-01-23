d864554406e6c397f83a02d165a7dd7b5b513ed2
TheMaryJacobs <TheMaryJacobs@gmail.com>
2020-01-23 13:34:19 -0600

    add mouse movement to paddle & make salmon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Paddle from "./components/Paddle";
 
 export default function App() {
+  const [paddleX, setPaddleX] = useState(0);
+  function handleMouse(e) {
+    console.log(e);
+    setPaddleX(e.x);
+  }
   return (
-    <div className="App">
-      <Paddle />
+    <div className="container" onMouseMove={handleMouse}>
+      <Paddle paddle={paddleX} />
     </div>
   );
 }
